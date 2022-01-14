36f7ac10354ea75c4b5829e7301b9d267ccc09d0
Allana Karla <allanakarla1702@gmail.com>
2022-01-14 15:23:40 -0300

    Iniciando projeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createProductImageElement(imageSource) {
+function createProductImageElement(imageSource) { // oii
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
