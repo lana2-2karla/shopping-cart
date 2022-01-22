fba7dfc740e22b5a6f10f2ef4f56f1d7c37271a3
Allana Karla <allanakarla1702@gmail.com>
2022-01-22 00:01:10 -0300

    resolve erro no teste saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -6,12 +6,12 @@ localStorageSimulator('setItem');
 describe('4 - Teste a função saveCartItems', () => {
   it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
     // Use .toHaveBeenCalled para garantir que uma função de simulação (mock, em inglês) foi chamada
-    saveCartItems(<ol><li>Item</li></ol>);
+    saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
   it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro cartItems e o segundo sendo o valor passado como argumento para saveCartItems', () => {
   
-    saveCartItems(<ol><li>Item</li></ol>);
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems',<ol><li>Item</li></ol>)
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems','<ol><li>Item</li></ol>')
   });
 });
